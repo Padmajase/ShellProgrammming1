bb5ee3bd94ca69afa03876371b30fa4c94f09839
Padmajase <padmajapawar7@gmail.com>
2022-03-20 16:39:12 +0530

    UC2

--- Employeewage.sh
+++ Employeewage.sh
@@ -2,14 +2,14 @@
 echo "hello world"
 ispresent=1
 isabsent=0
-wageperhour=20
+wageperhr=20
 if [ $((RANDOM%2)) -eq $ispresent ]
 then
 	echo "employee is present"
-	dailywage=$(($wageperhour*8))
+	dailywage=$(($wageperhr*8))
 	echo "$dailywage"
 else
 	echo "employee is absent"
-	dailywage=$(($wageperhour*0))
+	dailywage=$(($wageperhr*0))
 	echo "$dailywage"
 fi
