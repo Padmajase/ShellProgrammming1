d2de3738e3a004e80c6a094d051d1205db00e9f6
Padmajase <padmajapawar7@gmail.com>
2022-03-20 17:18:41 +0530

    Calculatin Wages till a condition

---
+++ UC6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+echo "hello world"
+ispresent=1
+isabsent=0
+parttime=2
+wageperhr=20
+fullworkhr=8
+partworkhr=8
+workdaypermonth=20
+
+x=$((RANDOM%3))
+while [ $dailywage != 3200 ]
+do
+case $x in
+	1)echo "employee is present"
+		dailywage=$(($workdaypermonth*$wageperhr*$fullworkhr))
+		echo "wage=$dailywage";;
+	0)echo "employee is absent"
+		dailywage=$(($wageperhr*0))
+		echo "wage=$dailywage";;
+	2)echo "employee is parttime"
+		dailywage=$(($workdaypermonth*$partworkhr*$wageperhr))
+		echo "wage=$dailywage";;
+	*)echo "invalid";;
+esac
+done
